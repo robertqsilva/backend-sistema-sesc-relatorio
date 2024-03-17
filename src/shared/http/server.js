b3376ae33c3eab3e6da4da0c4677b9d95b9aeff1
Robert Queiroz <robertqsilva1@gmail.com>
2024-03-17 15:24:11 -0300

    fix: removendo clusters para reduzir latencia

--- src/shared/http/server.js
+++ src/shared/http/server.js
@@ -4,45 +4,28 @@ config();
 import express from "express";
 import router from "./router/index.js";
 import cors from "cors";
-import cluster from "cluster";
 import cron from "node-cron";
 import axios from "axios";
 
-import { cpus } from "os";
-const qntdCpus = cpus().length;
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
-if (cluster.isPrimary) {
-  if (qntdCpus <= 1) {
-    cluster.fork();
-    cluster.fork();
-  } else {
-    for (let i = 0; i < qntdCpus; i++) {
-      cluster.fork();
-    }
-  }
+const PORT = process.env.PORT || 7777;
 
-  cluster.on("exit", (worker, code, singal) => {});
-} else {
-  const app = express();
-  app.use(express.json());
-  app.use(cors());
-  app.use(router);
+cron.schedule("*/2 * * * *", () => {
+  axios
+    .get("https://backend-sistema-sesc-relatorio.onrender.com/")
+    .then((response) => {
+      console.log("Solicitação de manutenção enviada com sucesso");
+    })
+    .catch((error) => {
+      console.error(
+        "Erro ao enviar a solicitação de manutenção:",
+        error.message
+      );
+    });
+});
 
-  const PORT = process.env.PORT || 7777;
-
-  cron.schedule("*/2 * * * *", () => {
-    axios
-      .get("https://backend-sistema-sesc-relatorio.onrender.com/")
-      .then((response) => {
-        console.log("Solicitação de manutenção enviada com sucesso");
-      })
-      .catch((error) => {
-        console.error(
-          "Erro ao enviar a solicitação de manutenção:",
-          error.message
-        );
-      });
-  });
-
-  app.listen(PORT);
-}
+app.listen(PORT);
